2337c99153cb09cda92d9658139f0bf79455432d
iswa <wala.benbrahim@esprit.tn>
2019-11-16 03:48:22 +0100

    Adding Maven Project

---
+++ src/main/java/exam/Test.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Arctic, today it's your project Assessement, so let's start!!");
+
+	}
+
+}
